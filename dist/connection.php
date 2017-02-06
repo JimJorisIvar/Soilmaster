56248468c02b3ca913ee5d088a173e89e2b010fe
jorisebbelaar <joris-ebbelaar@hotmail.nl>
2017-02-06 15:28:49 +0100

    test push

--- dist/connection.php
+++ dist/connection.php
@@ -1,8 +1,24 @@
 <?php
 
+/**
+ * Configuration for: Database Connection
+ *
+ * For more information about constants please @see http://php.net/manual/en/function.define.php
+ * If you want to know why we use "define" instead of "const" @see http://stackoverflow.com/q/2447791/1114320
+ *
+ * DB_HOST: database host, usually it's "127.0.0.1" or "localhost", some servers also need port info
+ * DB_NAME: name of the database. please note: database and database table are not the same thing
+ * DB_USER: user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
+ * DB_PASS: the password of the above user
+ */
+define("DB_HOST", "localhost");
+define("DB_NAME", "login");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+
 $DB_host = "localhost";
 $DB_user = "root";
-$DB_pass = "";
+$DB_pass = "root";
 $DB_name = "soilmaster";
 
 
@@ -16,5 +32,3 @@ catch(PDOException $e)
     echo $e->getMessage();
 }
 
-
-?>

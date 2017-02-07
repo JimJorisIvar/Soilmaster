30105a2d16847f7316edd7f1713d1ca7d44b4b97
Skievar <skievar@gmail.com>
2017-02-07 12:51:38 +0100

    db pass edit and ignore

--- dist/connection.php
+++ dist/connection.php
@@ -14,7 +14,7 @@
 define("DB_HOST", "localhost");
 define("DB_NAME", "login");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "root");
 
 $DB_host = "localhost";
 $DB_user = "root";
